d50067bdbbc90419a737a20752b3049e4e81ee31
yairshe <26732763+YairShemer@users.noreply.github.com>
2021-12-28 16:38:41 +0200

    save output (using hook)

--- tools/yair/run_demo.py
+++ tools/yair/run_demo.py
@@ -1,5 +1,9 @@
+import numpy as np
+
 from mmdet.apis import init_detector, inference_detector
 from plot_utils import plot_tensor
+from tools.yair.save_output import SaveOutput
+
 import sys
 import torch
 from tools.yair.FeatureExtractor import FeatureExtractor
@@ -29,12 +33,12 @@ device = 'cuda:0'
 model = init_detector(config_file, checkpoint_file, device=device)
 
 layers_names = get_layers_names(model)
-
+layers_dict = get_layers_dict(model)
 #plot_layer_filter(model, 'backbone.conv1')
 plot_layer_filter(model, 'backbone.layer1.0.conv2')
 
-
-
+save_output = SaveOutput()
+layers_dict['rpn_head.rpn_cls'].register_forward_hook(save_output)
 #resnet_features = FeatureExtractor(model, layers=["backbone.conv1"])
 #dummy_input = torch.ones(10, 3, 224, 224)
 #features = resnet_features(dummy_input)
@@ -50,5 +54,30 @@ with open('/home/yairshe/results/mmdetection_hackathon/onnx/model_layers.txt', '
         f.write(f'{name}\n')
         f.write(f'{str(param.shape)}\n\n')
 
+
+
 # inference the demo image
 results = inference_detector(model, '/home/yairshe/projectes/mmdetection/demo/demo.jpg')
+# plot_tensor(save_output.outputs)
+# plt.imshow(features['backbone.conv1'][0, 0, ...].cpu().numpy().astype(np.uint8))
+x = save_output.outputs[0][0, ...].cpu().numpy()
+x = np.transpose(x, (1,2,0))
+x = -x
+x[..., 0] = (x[..., 0] + x[..., 1] + x[..., 2 ]) / 3
+x[..., 1] = x[..., 2 ] = np.zeros(x[..., 0].shape)
+x = x.astype(np.uint8)
+# plt.imshow(x)
+# plt.show()
+from PIL import Image
+rpn_cls_output = Image.fromarray(x)
+l = rpn_cls_output.resize(image.size)
+image = Image.open('/home/yairshe/projectes/mmdetection/demo/demo.jpg')
+# l = l.convert('L')
+image_with_rpn_cls = Image.blend(image, l, 0.5)
+image_with_rpn_cls.show()
+
+l.paste(image)
+image.size
+Image.Image.paste(image, l)
+
+pass

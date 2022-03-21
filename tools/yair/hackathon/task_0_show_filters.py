bc0b5a16a0416432167ade48d62d7f26ce264e47
ep <ep@no.mail>
2022-03-21 13:04:34 +0200

    code cleanup

--- tools/yair/hackathon/task_0_show_filters.py
+++ tools/yair/hackathon/task_0_show_filters.py
@@ -8,25 +8,17 @@ import os
 device = 'cuda:0'
 MMDET_DIR = hku.get_mmdet_root()
 RESULTS_ROOT_DIR = MMDET_DIR + '/../../results/'
+
 config_file_path = MMDET_DIR + '/configs/faster_rcnn/faster_rcnn_r50_fpn_1x_coco.py'
 input_image_path = MMDET_DIR + '/demo/demo.jpg'
 output_dir_root_path = RESULTS_ROOT_DIR + '/task_0_show_filters'
 
 model = init_detector(config_file_path, checkpoint=None, device=device)
 
-layer_name = 'backbone.layer1.0.conv1.weight'
-layer_name = 'backbone.layer1.0.conv2'
-layer_name = 'backbone.layer1.2.conv3'
-layer_name = 'backbone.layer1.2.conv2'
-
 layers_dict = get_layers_dict(model)
 layers_names = list(layers_dict.keys())
-
-
 output_dir_path = os.path.join(output_dir_root_path, 'before_init')
-for layer_name in layers_names[1:]:
-    if '.bn' in layer_name or 'downsample' in layer_name:
-        continue
+for layer_name in [l for l in layers_names if '.bn' not in l and 'downsample' not in l and l != '']:
     output_file_path = os.path.join(output_dir_path, f'{layer_name}.png')
     plot_layer_filter(model, layer_name, show=False, output_file_path=output_file_path)
 
@@ -37,9 +29,7 @@ checkpoint_file_path = MMDET_DIR + '/checkpoints/faster_rcnn_r50_fpn_1x_coco_202
 model = init_detector(config_file_path, checkpoint=checkpoint_file_path, device=device)
 
 output_dir_path = os.path.join(output_dir_root_path, 'after_init')
-for layer_name in layers_names[1:]:
-    if '.bn' in layer_name or 'downsample' in layer_name:
-        continue
+for layer_name in [l for l in layers_names if '.bn' not in l and 'downsample' not in l and l != '']:
     output_file_path = os.path.join(output_dir_path, f'{layer_name}.png')
     plot_layer_filter(model, layer_name, show=False, output_file_path=output_file_path)
 

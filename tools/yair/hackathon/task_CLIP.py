daf3221665969b4b9c22bba12fc8c9bedd9094fa
ep <ep@no.mail>
2022-03-22 14:12:00 +0200

    CLIP + animals list

--- tools/yair/hackathon/task_CLIP.py
+++ tools/yair/hackathon/task_CLIP.py
@@ -1,5 +1,7 @@
+import glob
 import shutil
 import PIL.ImageShow
+from PIL import ImageDraw
 import numpy as np
 import torch
 import clip
@@ -9,7 +11,9 @@ from tools.yair.save_output import SaveOutput
 from tools.yair.plot_utils import plot_tensor_ontop_image
 from tools.yair.visualization_utils import get_layers_dict
 import tools.yair.hackathon.hackathon_utils as hku
+import animal_names
 import os
+
 
 
 # Show filters before loading the trained net:
@@ -18,27 +22,46 @@ MMDET_DIR = hku.get_mmdet_root()
 RESULTS_ROOT_DIR = MMDET_DIR + '/../../results/'
 
 config_file_path = MMDET_DIR + '/configs/faster_rcnn/faster_rcnn_r50_fpn_1x_coco.py'
-input_image_path = '/home/eyalper/hackathon2022/data/depositphotos_93215318-stock-photo-brown-bear-and-other-asian.jpg'
 output_dir_root_path = RESULTS_ROOT_DIR + '/task_CLIP'
+patches_out_dir=os.path.join(output_dir_root_path, 'patches')
 
-checkpoint_file_path = MMDET_DIR + '/checkpoints/faster_rcnn_r50_fpn_1x_coco_20200130-047c8118.pth'
-model = init_detector(config_file_path, checkpoint_file_path, device=device)
-results = inference_detector(model, input_image_path)
-show_result_pyplot(model, input_image_path, results, patches_out_dir=os.path.join(output_dir_root_path, 'patches'))
-###
-"""
-device = "cuda" if torch.cuda.is_available() else "cpu"
-model, preprocess = clip.load("ViT-B/32", device=device)
+def generate_patches():
+    input_image_path = '/home/eyalper/hackathon2022/data/depositphotos_93215318-stock-photo-brown-bear-and-other-asian.jpg'
+    checkpoint_file_path = MMDET_DIR + '/checkpoints/faster_rcnn_r50_fpn_1x_coco_20200130-047c8118.pth'
+    model = init_detector(config_file_path, checkpoint_file_path, device=device)
+    results = inference_detector(model, input_image_path)
+    show_result_pyplot(model, input_image_path, results,  patches_out_dir=patches_out_dir)
 
-image = preprocess(Image.open("CLIP.png")).unsqueeze(0).to(device)
-text = clip.tokenize(["a diagram", "a dog", "a cat"]).to(device)
 
-with torch.no_grad():
-    image_features = model.encode_image(image)
-    text_features = model.encode_text(text)
+def run_clip_on_patches_dir():
+    classes_for_clip = animal_names.animals_list
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    model, preprocess = clip.load("ViT-B/32", device=device)
 
-    logits_per_image, logits_per_text = model(image, text)
-    probs = logits_per_image.softmax(dim=-1).cpu().numpy()
+    for input_image_path in glob.glob(patches_out_dir + '/*.png'):
+        img = Image.open(input_image_path)
+        image = preprocess(img).unsqueeze(0).to(device)
+        text = clip.tokenize(classes_for_clip).to(device)
 
-print("Label probs:", probs)  # prints: [[0.9927937  0.00421068 0.00299572]]
-"""
+        with torch.no_grad():
+            image_features = model.encode_image(image)
+            text_features = model.encode_text(text)
+
+            logits_per_image, logits_per_text = model(image, text)
+            probs = logits_per_image.softmax(dim=-1).cpu().numpy()
+
+        max_prob_index = np.argmax(probs)
+        class_name = classes_for_clip[max_prob_index]
+        class_prob = probs[0, max_prob_index]
+        class_prob_txt = f'Class: {class_name}\nProbability: {class_prob}'
+        patch_name = os.path.basename(input_image_path)
+        print(f'Image: {patch_name}, {class_prob_txt}' )
+        ImageDraw.Draw(img).text((0,0), class_prob_txt, (50,50,50))
+        os.makedirs(patches_out_dir + '/../after_CLIP/', exist_ok=True)
+        img.save(patches_out_dir + '/../after_CLIP/' + patch_name)
+
+
+
+
+if __name__ == '__main__':
+    run_clip_on_patches_dir()
